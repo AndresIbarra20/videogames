a5afb4d95e38de00eb8895efd21f408946b1b84b
Andres <ibarravillamilandresfelipe@gmail.com>
2024-08-23 21:01:22 -0500

    Script python 1

---
+++ Scripts python/example 1.py
@@ -0,0 +1,11 @@
+#Este es un comentario de liinea#
+'''esto es un comentario de bloque'''
+data=''' esto e suna variablr dtring rn bloque'''
+n1=10
+n2=20
+suma= n1+n2
+print(suma)
+print("la suma es :",suma)
+print(f"la suma es : {suma}")
+print("la suma es :",n1+n2)
+
